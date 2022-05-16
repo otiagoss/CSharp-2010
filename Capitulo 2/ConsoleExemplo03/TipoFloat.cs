b987486602cb73979a79c1cb3ac2246061b0e0be
Tiago S Silva <tiago.souzasilva@gmail.com>
2022-05-15 22:37:57 -0300

    Aplicação do tipo Console criado no Capitulo 2
    Tipos Float

---
+++ Capitulo 2/ConsoleExemplo03/TipoFloat.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleExemplo03
+{
+    
+    class TipoFloat
+    {
+
+        static void Linha()
+        {
+
+            for (int linha = 1; linha <= 40; linha += 1)
+            {
+
+                Console.Write("-");
+
+            }
+
+        }
+        static void Main(string[] args)
+        {
+
+            int x = 3;
+            float y = 4.5F;
+            short z = 5;
+            Linha();
+            Console.WriteLine("\nO Resultado :{0}", x * y / z);
+            Linha();
+            Console.Write("\n\nPressione qualquer tecla...\n\n");
+            Linha();
+            Console.ReadKey();
+
+        }        
+
+    }
+
+}
