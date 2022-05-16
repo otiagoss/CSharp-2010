2ec8c4f305eb6cc45ed3bbb277c64bc3e453b69d
Tiago S Silva <tiago.souzasilva@gmail.com>
2022-05-15 22:21:27 -0300

    Aplicação do tipo Console criado no Capitulo 2 Tipos inteiros

---
+++ Capitulo 2/ConsoleExemplo02/TiposInteiros.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleExemplo02
+{
+    class TiposInteiros
+    {
+
+        static void Linha()
+        {
+
+            for (int linha = 1; linha <= 40; linha++)
+            {
+
+                Console.Write("-");
+
+            }
+
+        }
+        static void Main(string[] args)
+        {
+
+            byte idade1 = 35;
+            sbyte idade2 = 40;
+            short valor1 = 30000;
+            ushort valor2 = 65535;
+            int pontos1 = 10000;
+            uint pontos2 = 30000;
+            ulong contador = 2000000;
+            long contagem = 8000000;
+            Linha();
+            Console.SetCursorPosition(10, 1);
+            Console.Write("Tipos Inteiros em C#\n");
+            Linha();
+            Console.WriteLine("\n\nVariável do tipo byte : {0}", idade1);
+            Linha();
+            Console.WriteLine("\n\nVariável do tipo sbyte : {0}", idade2);
+            Linha();
+            Console.WriteLine("\n\nVariável do tipo short : {0}", valor1);
+            Linha();
+            Console.WriteLine("\n\nVariável do tipo ushort : {0}", valor2);
+            Linha();
+            Console.WriteLine("\n\nVariavel do tipo int : {0}", pontos1);
+            Linha();
+            Console.WriteLine("\n\nVariável do tipo uint : {0}", pontos2);
+            Linha();
+            Console.WriteLine("\n\nVariável do tipo ulong: {0}", contador);
+            Linha();
+            Console.WriteLine("\n\nVariável do tipo long: {0}", contagem);
+            Linha();
+            Console.WriteLine("\n\nPressione qualquer tecla ... \n\n");
+            Linha();
+            Console.ReadKey();
+
+        }
+
+    }
+
+}
