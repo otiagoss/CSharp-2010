b4fded98f9f381d785d4be32748860ad0cac73b1
Tiago S Silva <tiago.souzasilva@gmail.com>
2022-05-16 22:07:49 -0300

    Aplicação do tipo Console criado no Capitulo 2
    Tipos Double

---
+++ Capitulo 2/ConsoleExemplo05/TipoDouble.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleExemplo05
+{
+
+    class TipoDouble
+    {
+        static void Linha()
+        { 
+        
+            for(int linha = 1; linha <= 30; linha += 1)
+            {
+
+                Console.Write("-");
+
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            int x = 3;
+            float y = 4.5f;
+            short z = 5;
+            double w = 1.7E+3;
+            Linha();
+            Console.SetCursorPosition(8, 1);
+            Console.Write("Tipo souble em C#\n");
+            Linha();
+            Console.Write("\nO Resultado :{0}", x + y + z + w +"\n");
+            Linha();
+            Console.Write("\n\nPressione qualquer tecla.... \n\n");
+            Linha();
+            Console.ReadKey();
+
+        }
+
+    }
+
+}
+
